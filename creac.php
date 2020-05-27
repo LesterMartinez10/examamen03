8bc1f41c6f10ab751c795acee8cb21436d7382bd
Lester Orlando Martinez Hernández <201900062@colsanjose.edu.gt>
2020-05-27 14:49:42 -0600

    lester

---
+++ creac.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <title>Crear Usuario</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>       
+        <br />
+        <div align="center">
+            <h1 class="card-title"><span class="badge badge-secondary">Crear Usuario </span></h1>
+        </div>
+          <div align="right">          
+             <button type="button" class="btn btn-light">
+		     <a href="Verus.php">Ver Usuarios</a>
+             <button type="button" class="btn btn-light">
+		     <a href="index.html">salir del sistema</a>  
+        </div>
+        <body class="p-3 mb-2 bg-light text-dark" background="le.jpg">
+
+
+
+        </body>
+
+    </body>
+</html>
+
---
+++ user.php
@@ -0,0 +1,24 @@
+ <?php
+         include('validaruser.php');
+         $validar = new validarusuario;
+       
+
+
+         $nombre=$_POST['Nombre'];
+         $password=$_POST['Password'];
+
+         $resultado=$validar->validaruser($nombre,$password);
+ 
+
+    
+         if ($resultado->num_rows== 1) 
+         {
+           header("location:creac.php");
+                               
+         }
+         else 
+         {
+	        header("location:index.html");
+                     
+         }
+?>
